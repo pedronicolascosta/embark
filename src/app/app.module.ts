bbdb49b3a569eaa734a4a46453d2c9f108342baa
Pedro Nicolas Costa <pedroniickolas@gmail.com>
2025-07-14 00:44:00 -0300

    Projeto Angular inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeroComponent } from './hero/hero.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { TimeComponent } from './time/time.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeroComponent,
+    SobreComponent,
+    TimeComponent
   ],
   imports: [
     BrowserModule,
